0b61449813e419758c7dc7228f5f17ed1bdbff1b
George660 <76912644+George660@users.noreply.github.com>
2021-03-26 21:10:02 +0100

    Updated the getStaticProps

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const baseUrl = dev
   ? "http://localhost:3000"
-  : "https://afrotrap-main.vercel.app";
+  : "http://afrotrap-main.vercel.app";
 
 // export const server = dev ? "" : process.env.NEXT_PUBLIC_SERVER;
