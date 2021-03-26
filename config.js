84a7eaa1fc997507b99d496415e987c6bc1fa57a
George660 <76912644+George660@users.noreply.github.com>
2021-03-26 19:29:07 +0100

    Updated the getStaticProps

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const baseUrl = dev
   ? "http://localhost:3000"
-  : "https://vercel.com/apiprincz";
+  : process.env.NEXT_PUBLIC_SERVER;
 
-export const server = dev ? "" : process.env.NEXT_PUBLIC_SERVER;
+// export const server = dev ? "" : process.env.NEXT_PUBLIC_SERVER;
