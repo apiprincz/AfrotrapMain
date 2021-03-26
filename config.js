288076e8a7591ad480136c381b07c7265ee1281b
George660 <76912644+George660@users.noreply.github.com>
2021-03-26 19:48:04 +0100

    Updated the getStaticProps

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const baseUrl = dev
   ? "http://localhost:3000"
-  : process.env.NEXT_PUBLIC_SERVER;
+  : `http://${process.env.NEXT_PUBLIC_SERVER}`;
 
 // export const server = dev ? "" : process.env.NEXT_PUBLIC_SERVER;
