c9362b067a028e406317c3f7d754660afa799a54
George660 <76912644+George660@users.noreply.github.com>
2021-03-27 17:05:45 +0100

    fs error resolved

--- next.config.js
+++ next.config.js
@@ -21,14 +21,11 @@ module.exports = {
   //   };
   //   return config;
   // },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, fs, webpack }
-  ) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    config.node.isServer = {
+    config.node = {
       fs: true,
     };
 
